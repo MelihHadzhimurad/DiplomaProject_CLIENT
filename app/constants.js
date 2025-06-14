dcfe3a3e118fdbe33e3dc03553020225adf7865a
Melih <melihchohacimurat10@gmail.com>
2025-06-14 23:01:12 +0300

    Adding style attributes

---
+++ app/constants.js
@@ -0,0 +1,10 @@
+export const colors = {
+    background_color: '#caf0f8',
+    button_color: "#00b4d8",
+    button_text_color: "#03045e",
+    primary_text_color: "#0074B7",
+    secondary_text_color: "#0C4160",
+    font_size_large: 40,
+    font_size_medium: 30,
+    font_size_small: 18,
+};
---
+++ app/globalStyles.js
@@ -0,0 +1,55 @@
+import { StyleSheet } from "react-native";
+import { colors } from "./constants";
+
+export const styles = StyleSheet.create({
+    container: {
+        height: "100%",
+        backgroundColor: colors.background_color,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    header: {
+        margin: 20,
+        color: colors.secondary_text_color,
+        fontWeight: "bold",
+        fontSize: 20,
+        textAlign: "center"
+    },
+
+    button: {
+        backgroundColor: colors.button_color,
+        width: '50%',
+        height: '10%',
+        justifyContent: "center",
+        margin: 10,
+        borderRadius: 25,
+    },
+
+    button_text: {
+        color: colors.button_text_color,
+        textAlign: "center",
+        fontSize: 15,
+        fontWeight: "600"  
+    },
+
+    input_fields: {
+        borderColor: " #003459",
+        borderWidth: 2,
+        width: "70%",
+        height: "10%",
+        borderRadius: 25,
+        marginBottom: 20
+    },
+
+    input_headers: {
+        color: colors.button_text_color,
+        fontSize: 15,
+        fontWeight: "600",
+        width: "70%",
+        textAlign: "left",
+        paddingLeft: 20
+    }
+});
