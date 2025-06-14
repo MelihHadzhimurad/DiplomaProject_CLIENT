167b94b8ee3d83b756d12dd9760425ff1c39cbaf
Melih <melihchohacimurat10@gmail.com>
2025-06-14 22:51:53 +0300

    Constructing first page and customizing header bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
+import { colors } from "./constants";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack
+    screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.button_color,
+          
+        },
+        headerTitleAlign: "center",
+        headerTitleStyle: {
+          fontWeight: "bold"
+        },
+        headerTintColor: colors.secondary_text_color
+      }}
+  />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,39 @@
-import { Button } from "@react-navigation/elements";
-import { Text, View } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { Pressable, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "./globalStyles";
+
+export const screenOptions = {
+  headerShown: true,
+  headerTitle: 'Начална страница',
+  headerStyle: { backgroundColor: '#6200ee' },
+  headerTintColor: '#fff',
+};
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{
-        margin: 10,
-      }}>Изберете опция:</Text>
-      <Button>Вход</Button>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Stack.Screen
+        options={{
+          title: "Начална страница"
+        }}
+      />
+      
+      <Text style={styles.header}>Добре дошли в онлайн платформата на градска мобилност!</Text>
+      
+      <Pressable
+        style={styles.button}
+        onPress={() => router.push("/login/login")} >
+        <Text style={styles.button_text}>Вход</Text>
+      </Pressable>
+
+      <Pressable
+        style={styles.button}
+        onPress={() => router.push("/bracketCheck/bracketCheck")} >
+        <Text style={styles.button_text}>Проверка за поставена скоба</Text>
+      </Pressable>
+    </SafeAreaView>
   );
 }
